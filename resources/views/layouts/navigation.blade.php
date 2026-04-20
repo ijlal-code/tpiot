10181c7f5e6ae1ff3de999ad099f70d935fa2abf
Ainun Ijlal <ijlalcode@gmail.com>
2026-04-20 21:29:29 +0800

    update1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,6 +38,7 @@
         @endrole
             </div>
 
+            
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
