3259943285cc1384f4263832886cb1e74c568855
Ainun Ijlal <ijlalcode@gmail.com>
2026-04-24 17:42:56 +0800

    update2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
-import './bootstrap';
+import axios from 'axios';
+window.axios = axios;
 
-import Alpine from 'alpinejs';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.Alpine = Alpine;
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
 
-Alpine.start();
+import './echo'; // tambahkan ini
